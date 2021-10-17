bed150dd7154af2147a526f00aa1e3bd80e4889a
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-10-17 02:20:49 +0000

    Finish the useState tutorial

--- advanced-react-tutorial/src/App.jsx
+++ advanced-react-tutorial/src/App.jsx
@@ -4,14 +4,15 @@ import React from "react";
 // import ErrorExample from "./tutorial/1-useState/1-error-example";
 // import UseStateBasics from "./tutorial/1-useState/2-useState-basics"
 // import UseStateArray from "./tutorial/1-useState/3-useState-array"
-import UseStateObject from "./tutorial/1-useState/4-useState-object"
+// import UseStateObject from "./tutorial/1-useState/4-useState-object"
+import UseStateCounter from "./tutorial/1-useState/5-useState-counter";
 
 
 
 const App = () => {
   return (
     <div className="container">
-      <UseStateObject />
+      <UseStateCounter />
     </div>
   )
 }
---
+++ advanced-react-tutorial/src/tutorial/1-useState/5-useState-counter.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+const UseStateCounter = () => {
+
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0)
+  }
+
+  return <>
+    <section style={{margin: "4rem 0"}}>
+      <h2>regular counter</h2>
+      <h1>{value}</h1>
+    </section>
+
+    <button 
+      className="btn" 
+      onClick={() => setValue(value - 1)}>
+        Decrease
+    </button>
+    <button 
+      className="btn"
+      onClick={reset}
+      >
+        Reset
+      </button>
+    <button 
+      className="btn"
+      onClick={() => setValue(value + 1)}>
+        Increase
+      </button>
+  </>
+}
+
+
+export default UseStateCounter;
